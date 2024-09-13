87c775335c51ef7a0bf036f07a4cf012f373f24d
gabriellafguerra21 <gabriellafguerra21@gmail.com>
2024-09-13 17:20:11 +0000

    Added transacition routes

--- routes/web.php
+++ routes/web.php
@@ -19,12 +19,19 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
     Route::get('/balance', [BalanceController::class, 'index'])->name('balance.index');
-    Route::get('/balance/{date}', [BalanceController::class, 'dailyHistory'])->name('balance.daily');
+    Route::get('/balance/history', function () {
+        return view('balance.history');
+    })->name('balance.history');
+    Route::get('/transaction/history', function () {
+        return view('transaction.history');
+    })->name('transaction.history');
     Route::get('/transaction', [TransactionController::class, 'index'])->name('transaction.index');
     Route::get('/transaction/new', [TransactionController::class, 'create'])->name('transaction.create');
     Route::post('/transaction', [TransactionController::class, 'store'])->name('transaction.store');
     Route::get('/transaction/{id}', [TransactionController::class, 'show'])->name('transaction.show');
-
 });
+Route::middleware('auth:sanctum')->get('/api/balance/history', [BalanceController::class, 'balanceHistory']);
+Route::middleware('auth:sanctum')->get('/api/transaction/history', [TransactionController::class, 'transactionHistory']);
 
-require __DIR__.'/auth.php';
+
+require __DIR__ . '/auth.php';
